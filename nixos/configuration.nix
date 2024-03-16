b07f40d3cfa9769e1abe0df8b366107c8d1305bc
Andreas <andreas1990klo@hotmail.com>
2024-03-16 14:10:31 +0100

    removed unnecessary lines

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -22,17 +22,6 @@
   };
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  
-  nix.registry = (lib.mapAttrs (_: flake: {inherit flake;})) ((lib.filterAttrs (_: lib.isType "flake")) inputs); 
-  
-  nix.nixPath = ["/etc/nix/path"];
-  environment.etc =
-    lib.mapAttrs'
-    (name: value: {
-      name = "nix/path/${name}";
-      value.source = value.flake;
-    })
-    config.nix.registry;
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
