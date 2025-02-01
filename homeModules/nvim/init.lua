7fa8f7710ee5183e850eef210d568f920322811b
AndrezHP <usermail@mail.com>
2025-02-01 12:08:08 +0100

    lazydev, more snacks config, new buffer keymap

--- homeModules/nvim/init.lua
+++ homeModules/nvim/init.lua
@@ -1,7 +1,6 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.g.have_nerd_font = true -- If Nerd Font installed
-
 -- [[ Setting options ]] -- See `:help vim.opt`
 -- For more options, you can see `:help option-list`
 vim.opt.number = true
@@ -42,6 +41,9 @@ vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+
+-- Open and new file in insert mode
+vim.keymap.set("n", "<leader>nf", "<cmd>ene | startinsert<CR>", { desc = "Open new file in insert mode" })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- homeModules/nvim/lua/plugins/snacks.lua
+++ homeModules/nvim/lua/plugins/snacks.lua
@@ -9,8 +9,6 @@ return {
 		quickfile = { enabled = true },
 		statuscolumn = { enabled = true },
 		words = { enabled = true },
-		git = { enabled = true },
-		lazygit = { enable = true },
 	},
 	keys = {
 		{
@@ -20,13 +18,99 @@ return {
 			end,
 			desc = "Git blame for current line",
 		},
+
+		-- NOTE: trying extra config
+		{
+			"<leader>cR",
+			function()
+				Snacks.rename.rename_file()
+			end,
+			desc = "Rename File",
+		},
+		{
+			"<leader>gB",
+			function()
+				Snacks.gitbrowse()
+			end,
+			desc = "Git Browse",
+			mode = { "n", "v" },
+		},
+		{
+			"<leader>gb",
+			function()
+				Snacks.git.blame_line()
+			end,
+			desc = "Git Blame Line",
+		},
+		{
+			"<leader>gf",
+			function()
+				Snacks.lazygit.log_file()
+			end,
+			desc = "Lazygit Current File History",
+		},
 		{
 			"<leader>lg",
 			function()
 				Snacks.lazygit()
 			end,
-			desc = "Open lazygit in floating window",
+			desc = "Lazygit",
 		},
-		-- { "<leader>nf",  ":ene | startinsert", desc = "Open a new file in insert mode" },
+		{
+			"<leader>gl",
+			function()
+				Snacks.lazygit.log()
+			end,
+			desc = "Lazygit Log (cwd)",
+		},
+		{
+			"<leader>S",
+			function()
+				Snacks.scratch.select()
+			end,
+			desc = "Select Scratch Buffer",
+		},
+		{
+			"<leader>N",
+			desc = "Neovim News",
+			function()
+				Snacks.win({
+					file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
+					width = 0.6,
+					height = 0.6,
+					wo = {
+						spell = false,
+						wrap = false,
+						signcolumn = "yes",
+						statuscolumn = " ",
+						conceallevel = 3,
+					},
+				})
+			end,
+		},
+		{
+			"<c-/>",
+			function()
+				Snacks.terminal()
+			end,
+			desc = "Toggle Terminal",
+			mode = { "n", "t" },
+		},
+		{
+			"]]",
+			function()
+				Snacks.words.jump(vim.v.count1)
+			end,
+			desc = "Next Reference",
+			mode = { "n", "t" },
+		},
+		{
+			"[[",
+			function()
+				Snacks.words.jump(-vim.v.count1)
+			end,
+			desc = "Prev Reference",
+			mode = { "n", "t" },
+		},
 	},
 }
