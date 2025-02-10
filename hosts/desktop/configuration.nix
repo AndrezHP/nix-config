a48bc2f10658aa6ea49a7c78dfaf0869e872a094
AndrezHP <usermail@mail.com>
2025-02-10 19:31:40 +0100

    delete module

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -1,14 +1,8 @@
-{
-  lib,
-  config,
-  pkgs,
-  ...
-}: {
+{config, pkgs, ...}: {
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
     ../../nixosModules/displayserver.nix
-    # ../../nixosModules/stylix.nix
     # ../../nixosModules/wayland.nix
   ];
 
@@ -16,29 +10,24 @@
     enable = true;
     polarity = "dark";
     base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
-    
     fonts = {
       serif = {
         package = pkgs.dejavu_fonts;
         name = "DejaVu Serif";
       };
-
       sansSerif = {
         package = pkgs.dejavu_fonts;
         name = "DejaVu Sans";
       };
-
       monospace = {
         package = pkgs.nerd-fonts.jetbrains-mono;
         name = "JetBrainsMono Nerd Font Mono";
       };
-
       emoji = {
         package = pkgs.noto-fonts-emoji;
         name = "Noto Color Emoji";
       };
     };
-
     cursor = {
       package = pkgs.bibata-cursors;
       name = "Bibata-Modern-Ice";
