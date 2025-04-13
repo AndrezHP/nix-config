4baa7b0ad11734b1091e4b7761f15be5ba6a1ad6
AndrezHP <usermail@mail.com>
2025-04-13 16:31:33 +0200

    keyboard remapping with kanata

--- nixosModules/kanata/kanata.nix
+++ nixosModules/kanata/kanata.nix
@@ -1,12 +1,15 @@
-{ lib, config, ... }:
-with lib; let
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+with lib;
+let
   cfg = config.nixosModules.kanata;
 in
 {
-  options.nixosModules.kanata = {
-    enable = mkEnableOption "Enable kanata";
-  };
-
+  options.nixosModules.kanata.enable = mkEnableOption "Enable kanata";
   config = mkIf cfg.enable {
     # Enable the uinput module
     boot.kernelModules = [ "uinput" ];
@@ -19,8 +22,10 @@ in
       KERNEL=="uinput", MODE="0660", GROUP="uinput", OPTIONS+="static_node=uinput"
     '';
 
-    # Ensure the uinput group exists
-    users.groups.uinput = { };
+    # Ensure the uinput group exists and that the service is in it
+    users.groups.uinput = {
+      members = [ "kanata-internalKeyboard.service" ];
+    };
 
     # Add the Kanata service user to necessary groups
     systemd.services.kanata-internalKeyboard.serviceConfig = {
@@ -32,8 +37,22 @@ in
 
     services.kanata = {
       enable = true;
-      extraDefCfg = "process-unmapped-keys yes";
-      config = builtins.readFile ./config.kbd;
+      package = pkgs.kanata;
+      keyboards.internalKeyboard = {
+        devices = [
+          "/dev/input/by-path/pci-0000:02:00.0-usb-0:6:1.0-event-kbd"
+          "/dev/input/by-path/pci-0000:02:00.0-usb-0:5:1.0-event-kbd"
+          "/dev/input/by-path/pci-0000:02:00.0-usb-0:6:1.1-event-kbd"
+          "/dev/input/by-path/pci-0000:02:00.0-usbv2-0:6:1.0-event-kbd"
+          "/dev/input/by-path/pci-0000:02:00.0-usbv2-0:5:1.0-event-kbd"
+          "/dev/input/by-path/pci-0000:02:00.0-usbv2-0:6:1.1-event-kbd"
+        ];
+
+        config = builtins.readFile ./config.kbd;
+      };
+    };
+    services.udev = {
+      packages = [ pkgs.kanata ];
     };
   };
 }
