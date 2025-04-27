e2812fea827a667b76ba2797a975d5aa2de28a55
AndrezHP <usermail@mail.com>
2025-04-27 13:44:03 +0200

    screenshot can now save or copy to clipboard

--- dotfiles/rofi/scripts/capture.sh
+++ dotfiles/rofi/scripts/capture.sh
@@ -2,18 +2,16 @@
 
 theme="$HOME/nix-config/dotfiles/rofi/capture.rasi"
 
-CHOICE=$(printf "active window\nactive monitor\nselect window\nselect region" | rofi -dmenu -p "Screenshot:" -theme ${theme})
-case ''${CHOICE} in
-    "active window")
-    hyprshot -m window -m active
-    ;;
-    "active monitor")
-    hyprshot -m output -m active
-    ;;
-    "select window")
-    hyprshot -m window
-    ;;
-    "select region")
-    hyprshot -m region
-    ;;
+choice=$(printf "active window\nactive monitor\nselect window\nselect region" | rofi -dmenu -p "Screenshot:" -theme ${theme})
+
+case ''$1 in
+    "-s") flag="-o ~/Media/Screenshots" ;;
+    "-c") flag="--clipboard-only" ;;
+esac
+
+case ''${choice} in
+    "active window") hyprshot -m window $flag -m active ;;
+    "active monitor") hyprshot -m output $flag -m active ;;
+    "select window") hyprshot -m window $flag ;;
+    "select region") hyprshot -m region $flag ;;
 esac
