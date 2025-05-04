ed95b107708536186fc5579ee1bb1e1cc85606ef
AndrezHP <usermail@mail.com>
2025-05-04 19:19:50 +0200

    making git modular

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -1,10 +1,17 @@
-{ ... }: {
-  programs.git = { 
-    enable = true;
-    userName = "AndrezHP";
-    userEmail = "usermail@mail.com";
-    extraConfig = {
-      init.defaultBranch = "main";
+{ lib, config, ... }:
+let
+  cfg = config.homeModules.git;
+in
+{
+  options.homeModules.git.enable = lib.mkEnableOption "Enable git with configuration";
+  config = lib.mkIf cfg.enable {
+    programs.git = {
+      enable = true;
+      userName = "AndrezHP";
+      userEmail = "usermail@mail.com";
+      extraConfig = {
+        init.defaultBranch = "main";
+      };
     };
   };
 }
