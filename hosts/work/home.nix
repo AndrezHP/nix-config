13f95fd132cac9562c766bfdccf4384990535dcd
AndrezHP <usermail@mail.com>
2025-05-04 19:23:32 +0200

    initial home manager config for work

---
+++ hosts/work/home.nix
@@ -0,0 +1,57 @@
+{ pkgs, ... }:
+{
+  imports = [
+    ../../modules/home
+  ];
+
+  homeModules = {
+    emacs.enable = true;
+    zsh = {
+      enable = true;
+      extraAliases = {
+        bh = "home-manager switch --flake ~/nix-config/#work";
+      };
+    };
+    kitty.enable = true;
+    nvimConfig = {
+      enable = true;
+      setBuildEnv = true;
+      withBuildTools = true;
+    };
+    applications = {
+      cliTools.enable = true;
+      devTools.enable = true;
+    };
+  };
+
+  xdg.configFile."rofi".source = ../../dotfiles/rofi;
+  programs.home-manager.enable = true;
+
+  home.packages = with pkgs; [
+    jetbrains-toolbox
+    gcc
+    pgadmin4
+    pgadmin4
+    watchman
+  ];
+
+  nixpkgs = {
+    overlays = [ ];
+    config = {
+      allowUnfree = true;
+      allowUnfreePredicate = _: true;
+    };
+  };
+
+  home = {
+    username = "andreas";
+    homeDirectory = "/home/andreas";
+    sessionVariables = {
+      EDITOR = "nvim";
+    };
+  };
+
+  systemd.user.startServices = "sd-switch";
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "23.11";
+}
