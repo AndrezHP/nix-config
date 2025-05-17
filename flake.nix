43fbcadd04407b12af3ff6bfc0b7482d0f921236
AndrezHP <usermail@mail.com>
2025-05-17 17:16:05 +0200

    fix merge errors

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     sops-nix.url = "github:Mic92/sops-nix";
-    sops-nix.inputs.nnixpkgs.follows = "nixpkgs";
+    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
