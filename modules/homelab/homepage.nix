dd99f0dae4be928f878b5ec203f05366e680b065
AndrezHP <usermail@mail.com>
2025-06-22 16:12:01 +0200

    rethinking homepage configuration

--- modules/homelab/homepage.nix
+++ modules/homelab/homepage.nix
@@ -28,109 +28,108 @@ in
         statusStyle = "dot";
       };
 
-      services = [
-        {
-          Services = [
-            {
-              Vaultwarden = {
-                name = "Vaultwarden";
-              };
-            }
-            {
-              Uptime-Kuma = {
-                name = "Uptime Kuma";
-                description = "Service monitoring tool";
-                icon = " uptime-kuma.svg";
-                href = "http://192.168.1.223:3001";
-                siteMonitor = "http://localhost:3001";
-              };
-            }
+      services =
+        let
+          categories = [
+            "Media"
+            "Arr"
+            "Services"
           ];
-        }
-        {
-          Arr = [
-            {
-              Radarr = {
-                name = "Radarr";
-              };
-            }
-          ];
-        }
-        {
-          Media = [
-            {
-              jellyfin = {
-                name = "Jellyfin";
-                icon = "jellyfin.svg";
-                description = "The Free Software Media System";
-                href = "http://192.168.1.223:8096";
-                siteMonitor = "http://localhost:8096";
-              };
-            }
-            {
-              immich = {
-                name = "Immich";
-                icon = "immich.svg";
-                description = "Self-hosted photo and video management solution";
-                href = "http://192.168.1.223:2283";
-                siteMonitor = "http://localhost:2283";
-              };
-            }
-          ];
-        }
-        {
-          Glances =
-            let
-              port = toString config.services.glances.port;
-            in
-            [
+          filterServices =
+            x: (lib.attrsets.filterAttrs (name: value: value ? homepage && value.category == x) config.homelab);
+        in
+        lib.lists.forEach categories (cat: {
+          "${cat}" =
+            lib.lists.forEach (lib.attrsets.mapAttrsToList (name: value: name) (filterServices "${cat}"))
+              (x: {
+                "${config.homelab.${x}.homepage.name}" = config.homelab.${x}.homepage;
+              });
+        })
+        ++ [
+          {
+            Arr = [
               {
-                Info = {
-                  widget = {
-                    type = "glances";
-                    url = "http://localhost:${port}";
-                    metric = "info";
-                    chart = false;
-                    version = 4;
-                  };
-                };
-              }
-              {
-                "CPU Temp" = {
-                  widget = {
-                    type = "glances";
-                    url = "http://localhost:${port}";
-                    metric = "sensor:Package id 0";
-                    chart = false;
-                    version = 4;
-                  };
-                };
-              }
-              {
-                Processes = {
-                  widget = {
-                    type = "glances";
-                    url = "http://localhost:${port}";
-                    metric = "process";
-                    chart = false;
-                    version = 4;
-                  };
-                };
-              }
-              {
-                Network = {
-                  widget = {
-                    type = "glances";
-                    url = "http://localhost:${port}";
-                    metric = "network:enp2s0f1";
-                    chart = false;
-                    version = 4;
-                  };
+                Radarr = {
+                  name = "Radarr";
                 };
               }
             ];
-        }
-      ];
+          }
+          {
+            Media = [
+              {
+                jellyfin = {
+                  name = "Jellyfin";
+                  icon = "jellyfin.svg";
+                  description = "The Free Software Media System";
+                  href = "http://192.168.1.223:8096";
+                  siteMonitor = "http://localhost:8096";
+                };
+              }
+              {
+                immich = {
+                  name = "Immich";
+                  icon = "immich.svg";
+                  description = "Self-hosted photo and video management solution";
+                  href = "http://192.168.1.223:2283";
+                  siteMonitor = "http://localhost:2283";
+                };
+              }
+            ];
+          }
+          {
+            Glances =
+              let
+                port = toString config.services.glances.port;
+              in
+              [
+                {
+                  Info = {
+                    widget = {
+                      type = "glances";
+                      url = "http://localhost:${port}";
+                      metric = "info";
+                      chart = false;
+                      version = 4;
+                    };
+                  };
+                }
+                {
+                  "CPU Temp" = {
+                    widget = {
+                      type = "glances";
+                      url = "http://localhost:${port}";
+                      metric = "sensor:Package id 0";
+                      chart = false;
+                      version = 4;
+                    };
+                  };
+                }
+                {
+                  Processes = {
+                    widget = {
+                      type = "glances";
+                      url = "http://localhost:${port}";
+                      metric = "process";
+                      chart = false;
+                      version = 4;
+                    };
+                  };
+                }
+                {
+                  Network = {
+                    widget = {
+                      type = "glances";
+                      url = "http://localhost:${port}";
+                      metric = "network:enp2s0f1";
+                      chart = false;
+                      version = 4;
+                    };
+                  };
+                }
+              ];
+          }
+        ];
     };
   };
 }
--- modules/homelab/uptime-kuma.nix
+++ modules/homelab/uptime-kuma.nix
@@ -3,7 +3,23 @@ let
   cfg = config.homelab.uptime-kuma;
 in
 {
-  options.homelab.uptime-kuma.enable = lib.mkEnableOption "Enable Uptime Kuma";
+  options.homelab.uptime-kuma = {
+    enable = lib.mkEnableOption "Enable Uptime Kuma";
+    category = lib.mkOption {
+      type = lib.types.str;
+      default = "Services";
+    };
+    homepage = lib.mkOption {
+      type = lib.types.attrs;
+      default = {
+        name = "uptime-kuma";
+        description = "Service monitoring tool";
+        icon = " uptime-kuma.svg";
+        href = "http://192.168.1.223:3001";
+        siteMonitor = "http://localhost:3001";
+      };
+    };
+  };
   config = lib.mkIf cfg.enable {
     services.uptime-kuma.enable = true;
     networking.firewall.allowedTCPPorts = [ 3001 ];
