549f33077c0e77bf469e8da6c13ecc4ac391e426
AndrezHP <usermail@mail.com>
2025-07-03 21:44:13 +0200

    re-add immich host ip

--- modules/homelab/immich.nix
+++ modules/homelab/immich.nix
@@ -25,6 +25,7 @@ in
   config = lib.mkIf cfg.enable {
     services.immich = {
       enable = true;
+      host = "0.0.0.0";
       accelerationDevices = null;
     };
     users.users.immich.extraGroups = [
