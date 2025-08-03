4c6fdb551e9df93189f42fcd21c5f2440b3d8f25
AndrezHP <usermail@mail.com>
2025-08-03 14:09:08 +0200

    remove unnecessary theme settings

--- modules/home/theme.nix
+++ modules/home/theme.nix
@@ -8,12 +8,7 @@
   options.homeModules.theme.enable = lib.mkEnableOption "Enable theming of gtk/qt";
   config = lib.mkIf config.homeModules.theme.enable {
     home.packages = [ pkgs.arc-theme ];
-    dconf.settings."org/gnome/desktop/interface" = {
-      color-scheme = "prefer-dark";
-    };
-    home.sessionVariables = {
-      GTK_THEME = "Arc-Dark";
-    };
+    home.sessionVariables.GTK_THEME = "Arc-Dark";
     gtk = {
       enable = true;
       theme = {
@@ -28,14 +23,7 @@
         name = "Papirus-Dark";
         package = pkgs.papirus-icon-theme;
       };
-      gtk3.extraConfig = {
-        gtk-application-prefer-dark-theme = 1;
-      };
-      gtk4.extraConfig = {
-        gtk-application-prefer-dark-theme = 1;
-      };
     };
-
     qt = {
       enable = true;
       platformTheme.name = "gtk";
