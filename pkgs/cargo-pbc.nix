8e6961f141a30b8ae100cccfd7b140c813537768
Andreas HP <andreas.hoilund.pedersen@partisia.com>
2025-09-05 14:08:01 +0200

    update cargo-pbc

--- pkgs/cargo-pbc.nix
+++ pkgs/cargo-pbc.nix
@@ -1,7 +1,7 @@
 { pkgs, lib, ... }:
 pkgs.rustPlatform.buildRustPackage {
   pname = "cargo-pbc";
-  version = "5.191.0";
+  version = "5.265.0";
   buildInputs = [
     pkgs.openssl
   ];
@@ -15,14 +15,15 @@ pkgs.rustPlatform.buildRustPackage {
   src = pkgs.fetchFromGitLab {
     owner = "partisiablockchain";
     repo = "language/cargo-partisia-contract";
-    rev = "4d2568850dd86d2afd71d628be06df73d6d43430";
-    hash = "sha256-ZJ6+GLwNmSo7P5RhPSCtmpnLPf6zm9h3gMyMEYtgki0=";
+    rev = "1bb51a58970cdbefda874fdd5fdd4979004d901d";
+    hash = "sha256-m8ZEwb/DrW0NxXFsgn3Av+elZEGZnw6tVMuzGguJzgw=";
   };
-  cargoHash = "sha256-GGQ+1x/NaPfU3M+4FHxPg87+XUeLleLk8+2Rc1wzwCE=";
+  cargoHash = "sha256-BF0uQHb1Hq3jJt9E1Cd+RJkaney9ErD8/kEX4VVIftk=";
+
   meta = {
     description = "Compiles Smart Contracts for the Partisia Blockchain for deployment on-chain.";
     homepage = "https://gitlab.com/partisiablockchain/language/cargo-partisia-contract";
     license = lib.licenses.mit;
-    mainProgram = "cargo-pbc";
+    maintainers = [ ];
   };
 }
